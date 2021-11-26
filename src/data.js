8b879e9075aac36bf7c8ad2628e0c099b34f088c
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-26 10:44:50 -0500

    updated pictures and fixed up css styling a little

--- src/data.js
+++ src/data.js
@@ -1,10 +1,15 @@
 export const inventoryData = [
-  { name: 'Hello World Hat', price: 23.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg' },
-  { name: 'Learn From Home Joggers', price: 45.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sweatpants.jpg' },
-  { name: 'Java Tee', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/java-tee.jpg' },
-  { name: 'Python Tee', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/python-tee.jpg' },
-  { name: 'SQL Tee', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sql-tee.jpg' },
-  { name: 'Hello World Hoodie', price: 49.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hoodie.jpg' },
+  { name: 'BLACK Adidas Hoodie', price: 44.99, img: 'https://static.footshop.com/473536-full_product/35567.jpg' },
+  { name: 'Strawberry Cow 4ever Hoodie', price: 26.99, img: 'https://m.media-amazon.com/images/I/61R6Rp3haaL._AC_UX679_.jpg' },
+  { name: 'FUTURE WorderVote Panties', price: 9.69, img: 'https://cdn.shopify.com/s/files/1/0095/0511/2126/products/SMART-AND-SEXY-PINK-VOTING-PANTIES-SA1100_x800.jpg?v=1578406694' },
+  { name: 'Sandy Cheeks Tee', price: 14.99, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB1kYXQ0rXLo7LkXUESEtqaJhi3nPn8j7h1A&usqp=CAU' },
+  { name: 'Red Under Armour Boxers', price: 10.01, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8wSO6QLzC2ezWHJg-F-hqK7Q6AaxY1kEYQg&usqp=CAU' },
+  { name: 'Heritage Skate Helmet', price: 21.99, img: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTRK1ZfCmSywc2H38sG5vk66u8n_hoILe-qwCDzYIFUbNFqr6Hr17BFXs0TskdTzSn6j2MtuzCL35E&usqp=CAc' },
+  { name: 'Kyle Walker Skate Shoes', price: 69.69, img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT1m6isvl7OLFD-wBGYXbTKyaNb9_tPHg9-GPuezgwDCclvn97Ny45U-rROOtsYzXxf0JnxCy2euA&usqp=CAc' },
+  { name: 'Kyle Walker REAL Skate Deck', price: 60.00, img: 'https://tva1.sinaimg.cn/large/008i3skNgy1gwsx79tabxj30u00u0q7n.jpg' },
+  { name: 'Holy Bible KJV', price: 999.99, img: 'https://i5.walmartimages.com/asr/eaa4ccd9-a2c2-4b36-9b2f-c5254e35fe0c_1.117188d58aa6daec36cc0e46e0e8fb11.jpeg' },
+  { name: 'Ibanez White Electric Guitar', price: 777.00, img: 'https://media.guitarcenter.com/is/image/MMGS7/J48397000002000-02-720x720.jpg' },
+  { name: 'Play-Doh 50+', price: 666.00, img: 'https://m.media-amazon.com/images/I/91Qbeem6-hL._AC_SL1500_.jpg' }
 ];
 
 export const currenciesData = ['USD', 'EUR', 'CAD'];

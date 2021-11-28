bff4f0ee170f17ae90488918212bb5d4e09e13c3
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-28 14:35:04 -0500

    Updated Pncho to Poncho

--- src/data.js
+++ src/data.js
@@ -6,10 +6,14 @@ export const inventoryData = [
   { name: 'Red Under Armour Boxers', price: 10.01, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8wSO6QLzC2ezWHJg-F-hqK7Q6AaxY1kEYQg&usqp=CAU' },
   { name: 'Heritage Skate Helmet', price: 21.99, img: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTRK1ZfCmSywc2H38sG5vk66u8n_hoILe-qwCDzYIFUbNFqr6Hr17BFXs0TskdTzSn6j2MtuzCL35E&usqp=CAc' },
   { name: 'Kyle Walker Skate Shoes', price: 69.69, img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT1m6isvl7OLFD-wBGYXbTKyaNb9_tPHg9-GPuezgwDCclvn97Ny45U-rROOtsYzXxf0JnxCy2euA&usqp=CAc' },
-  { name: 'Kyle Walker REAL Skate Deck', price: 60.00, img: 'https://tva1.sinaimg.cn/large/008i3skNgy1gwsx79tabxj30u00u0q7n.jpg' },
+  { name: 'Kyle Walker REAL Skateboard', price: 60.00, img: 'https://tva1.sinaimg.cn/large/008i3skNgy1gwsx79tabxj30u00u0q7n.jpg' },
   { name: 'Holy Bible KJV', price: 999.99, img: 'https://i5.walmartimages.com/asr/eaa4ccd9-a2c2-4b36-9b2f-c5254e35fe0c_1.117188d58aa6daec36cc0e46e0e8fb11.jpeg' },
   { name: 'Ibanez White Electric Guitar', price: 777.00, img: 'https://media.guitarcenter.com/is/image/MMGS7/J48397000002000-02-720x720.jpg' },
-  { name: 'Play-Doh 50+', price: 666.00, img: 'https://m.media-amazon.com/images/I/91Qbeem6-hL._AC_SL1500_.jpg' }
+  { name: 'Play-Doh 50+', price: 666.00, img: 'https://m.media-amazon.com/images/I/91Qbeem6-hL._AC_SL1500_.jpg' },
+  { name: 'Mongoose Malus Adult Fat Tire 26-inch Wheels Mountain Bike', price: 400.00, img: 'https://m.media-amazon.com/images/I/91MqcxxSzFL._AC_SL1500_.jpg' },
+  { name: 'Psychedelic Smack Headband', price: 14.99, img: 'https://cdn.shopify.com/s/files/1/0971/6442/products/PSYCHEDELIC_SMACK_FT-S1_1920x.jpg?v=1583355713' },
+  { name: 'Poncho Multicolor Alpaca', price: 49.99, img: 'https://cdn.webshopapp.com/shops/305618/files/338151270/768x1024x3/ecuafina-native-poncho-yellow-multicolor-yellow-co.jpg' },
+  { name: 'The BUTTers Oil-Based Lube', price: 6.90, img: 'https://static.bvibe.com/media/catalog/product/cache/7/thumbnail/770x/0dc2d03fe217f8c83829496872af24a0/t/h/the-butters-oil-based-lube-1_2.jpg' }
 ];
 
 export const currenciesData = ['USD', 'EUR', 'CAD'];
